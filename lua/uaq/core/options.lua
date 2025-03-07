4e7b9fb8387e66aa8e60b3e5c20c42478e970270
Michael Oltman <trich.oltman@gmail.com>
2025-03-06 23:54:32 -0600

    updated keymaps; changed bufferline for cokeline

--- lua/uaq/core/options.lua
+++ lua/uaq/core/options.lua
@@ -8,8 +8,8 @@ opt.number = true -- shows absolute line number on cursor line (when relative nu
 vim.cmd("let g:netrw_liststyle = 3") -- directory style
 
 -- tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
